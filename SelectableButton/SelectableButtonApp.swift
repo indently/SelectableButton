49268ccf0b5f150f0dcaa2ba703af0c1ea9d730d
Code Palace <codepalace@fastmail.se>
2021-12-03 17:07:31 +0100

    Initial Commit

---
+++ SelectableButton/SelectableButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectableButtonApp.swift
+//  SelectableButton
+//
+//  Created by Federico on 03/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SelectableButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
